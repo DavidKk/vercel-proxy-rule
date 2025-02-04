0fef15fb03dd893ee272cf879c4b641f2abf98d2
DavidJones <qowera@gmail.com>
2025-02-04 22:12:35 +0800

    perf: update login expire time

--- app/clash/RuleManager.tsx
+++ app/clash/RuleManager.tsx
@@ -237,7 +237,7 @@ export default function RuleManager(props: RuleManagerProps) {
             className="px-4 py-2 bg-green-500 cursor-pointer text-sm text-white rounded-sm hover:bg-green-600 disabled:opacity-50 disabled:cursor-not-allowed"
             type="submit"
           >
-            {submitting ? <Spinner /> : 'Save'}
+            {submitting ? <span className="w-5 h-5 flex flex-row items-center"><Spinner /></span> : 'Save'}
           </button>
         </div>
       </footer>
